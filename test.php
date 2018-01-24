2febb853fe088d5062e652e56d1f21d3398f125d
OlesyaKornukova <34768179+OlesyaKornukova@users.noreply.github.com>
2018-01-24 11:24:24 +0300

    Update test.php

--- test.php
+++ test.php
@@ -39,32 +39,20 @@ if (count($_POST) > 0) {
 }
 ?>
 
-<!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Тест: <?=$question?></title>
-</head>
-<body>
-
-<form method="post">
-    <fieldset>
-        <legend><?=$question?></legend>
-        <?php foreach ($answers[0] as $key => $item) : ?>
-            <label><input type="radio" name="<?=$key;?>" value="<?=$item['answer'];?>"> <?=$item['answer'];?></label>
-        <?php endforeach; ?>
-    </fieldset>
-    <input type="submit" value="Отправить">
-</form>
-
-<ul>
-    <li><a href="admin.php">Загрузить тест</a></li>
-    <li><a href="list.php">Список тестов</a></li>
-</ul>
-
-</body>
-</html>
-
+<a href='admin.php'>Загрузить тест.</a><br>
+	<a href=list.php>Список загруженных тестов.</a>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Document</title>
+ </head>
+ <body>
+ 	<div align="center">
+ 	<ul>
+ 			 			<h3><a href="test.php?nameTest=test1.json">test-1.json</a></h3>
+ 	 			
+  	</ul>
+ 	</div>
+ </body>
+ </html>
